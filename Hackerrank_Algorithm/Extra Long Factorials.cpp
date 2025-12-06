21eab3cc15c03b71b499f80c7392fa8a7b381ca4
Mit-Solanki <mitsolanki004@gmail.com>
2025-12-06 23:32:33 +0530

    Add files via upload

---
+++ Hackerrank_Algorithm/Extra Long Factorials.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+void extraLongFactorials(int n) {
+    vector<int> result(1, 1);
+
+    for (int x = 2; x <= n; x++) {
+        int carry = 0;
+        for (int i = 0; i < result.size(); i++) {
+            int val = result[i] * x + carry;
+            result[i] = val % 10;
+            carry = val / 10;
+        }
+        while (carry) {
+            result.push_back(carry % 10);
+            carry /= 10;
+        }
+    }
+
+    for (int i = result.size() - 1; i >= 0; i--)
+        cout << result[i];
+}
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    extraLongFactorials(n);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
